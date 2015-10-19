1910a2245ee90798f1f073387b6e91d719443dfc
Robert Corty <rcorty@gmail.com>
2015-10-19 14:07:09 -0400

    prep get.peaks.from.varscan for inclusion in master
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/get.peaks.from.scanonevar.R
+++ R/get.peaks.from.scanonevar.R
@@ -1,14 +1,29 @@
-# function under development
-# idea is to find local maxima
+#'  @title Get Local Maxima from Scanonevar
+#'
+#'  @author Robert Corty \email{rcorty@@gmail.com}
+#'
+#'  @description \code{get.peaks.from.scanonevar} scans the genome for loci such that
+#'    the locus to the left has a lower value and the locus to the right has a lower value.
+#'    This value can be either LOD score or -log10(p-value)
+#'
+#'  @param x the \code{scanonevar} object from which the peaks will be identified
+#'  @param thresh Optionally, the threshold over which a value has to be to be considered a peak.
+#'    For example if one locus has a LOD score of 1 and the loci to its sides have LOD score of
+#'    0.9, that's not really an interesting or "peaky" locus.  Defaults to 3 if x is in LOD units
+#'    and 0.05 if x is in p-values
+#'
+#'  @return tbl_df of identified loci
+#'
+#'  @details none
+#'
 
+get.peaks.from.scanonevar <- function(x, thresh) {
 
-get.peaks.from.scanonevar <- function(vs, thresh) {
-
-  if (units(vs) == 'lods') {
+  if (units(x) == 'lods') {
 
     if (missing(thresh)) { thresh <- 3 }
 
-    peaks <- vs %>%
+    peaks <- x %>%
       mutate(full.peak = and(full.lod >= lag(full.lod, default = 0),
                              full.lod >= lead(full.lod, default = 0))) %>%
       mutate(mean.peak = and(mean.lod >= lag(mean.lod, default = 0),
@@ -17,11 +32,11 @@ get.peaks.from.scanonevar <- function(vs, thresh) {
                             var.lod >= lead(var.lod, default = 0)))
   }
 
-  if (units(vs) == 'emp.ps') {
+  if (units(x) == 'emp.ps') {
 
     if (missing(thresh)) { thresh <- 0.05 }
 
-    peaks <- vs %>%
+    peaks <- x %>%
       mutate(full.peak = and(emp.p.full.lod <= lag(emp.p.full.lod, default = 1),
                              emp.p.full.lod <= lead(emp.p.full.lod, default = 1))) %>%
       mutate(mean.peak = and(emp.p.mean.lod <= lag(emp.p.mean.lod, default = 1),
