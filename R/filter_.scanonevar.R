9c76535daf6c8c6fdbc67ef7336f493db76b57a3
Robert Corty <rcorty@gmail.com>
2015-10-19 11:41:37 -0400

    added functions under development
    Signed-off-by: Robert Corty <rcorty@gmail.com>

---
+++ R/filter_.scanonevar.R
@@ -0,0 +1,22 @@
+# function under development
+# custom filter function for scanonevar objects that will persist their attributes
+# need to figure out how to get this to work right
+
+filter_.scanonevar <- function(vs, ...) {
+
+  out <- vs
+  class(out) <- class(out)[-1]
+
+  out <- dplyr::filter_(out, ...)
+
+  class(out) <- class(vs)
+  attr(out, 'method') <- attr(vs, 'method')
+  attr(out, 'type') <- attr(vs, 'type')
+  attr(out, 'model') <- attr(vs, 'model')
+  attr(out, 'dom') <- attr(vs, 'dom')
+  attr(out, 'pheno') <- attr(vs, 'pheno')
+  attr(out, 'null.effects') <- attr(vs, 'null.effects')
+  attr(out, 'units') <- attr(vs, 'units')
+
+  return(out)
+}
