9a03d99bc5fe6f25c6e5cbb199e94232de4f0565
Robert Corty <rcorty@gmail.com>
2016-08-02 14:02:57 -0400

    comment out non-automated test1.R
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- tests/test1.R
+++ tests/test1.R
@@ -1,46 +1,46 @@
-######### Simulate mQTL and vQTL modeled with scanonevar
-library(qtl)
-library(vqtl)
-
-my.cross <- sim.cross(map = sim.map(len = rep(100, 4), n.mar = 30, eq.spacing = TRUE, include.x = FALSE),
-                      n.ind = 200,
-                      type = 'f2')
-my.cross$pheno$sex <- rep(x = c(0, 1), each = 100)
-my.cross <- calc.genoprob(my.cross)
-
-my.cross$pheno$phenotype1 <- rnorm(n = nind(my.cross))
-my.cross$pheno$phenotype2 <- rnorm(n = nind(my.cross), mean = 0.8*my.cross$geno$`1`$data[,15])
-my.cross$pheno$phenotype3 <- rnorm(n = nind(my.cross), sd = my.cross$geno$`2`$data[,15])
-my.cross$pheno$phenotype4 <- rnorm(n = nind(my.cross), mean = my.cross$geno$`3`$data[,15], sd = my.cross$geno$`3`$data[,15])
-
-
-a1 <- scanonevar(cross = my.cross,
-                 mean.formula = 'phenotype1 ~ sex + mean.QTL.add + mean.QTL.dom',
-                 var.formula = '~sex + var.QTL.add + var.QTL.dom',
-                 return.models = TRUE)$varscan
-a2 <- scanone(cross = my.cross,
-              pheno.col = 'phenotype1')
-
-b1 <- scanonevar(cross = my.cross,
-                 mean.formula = 'phenotype2 ~ sex + mean.QTL.add + mean.QTL.dom',
-                 var.formula = '~sex + var.QTL.add + var.QTL.dom',
-                 return.models = TRUE)$varscan
-b2 <- scanone(cross = my.cross,
-              pheno.col = 'phenotype2')
-
-c1 <- scanonevar(cross = my.cross,
-                 mean.formula = 'phenotype3 ~ sex + mean.QTL.add + mean.QTL.dom',
-                 var.formula = '~sex + var.QTL.add + var.QTL.dom',
-                 return.models = TRUE)$varscan
-c2 <- scanone(cross = my.cross,
-              pheno.col = 'phenotype3')
-
-d1 <- scanonevar(cross = my.cross,
-                 mean.formula = 'phenotype4 ~ sex + mean.QTL.add + mean.QTL.dom',
-                 var.formula = '~sex + var.QTL.add + var.QTL.dom',
-                 return.models = TRUE)$varscan
-d2 <- scanone(cross = my.cross,
-              pheno.col = 'phenotype4')
+# ######### Simulate mQTL and vQTL modeled with scanonevar
+# library(qtl)
+# library(vqtl)
+#
+# my.cross <- sim.cross(map = sim.map(len = rep(100, 4), n.mar = 30, eq.spacing = TRUE, include.x = FALSE),
+#                       n.ind = 200,
+#                       type = 'f2')
+# my.cross$pheno$sex <- rep(x = c(0, 1), each = 100)
+# my.cross <- calc.genoprob(my.cross)
+#
+# my.cross$pheno$phenotype1 <- rnorm(n = nind(my.cross))
+# my.cross$pheno$phenotype2 <- rnorm(n = nind(my.cross), mean = 0.8*my.cross$geno$`1`$data[,15])
+# my.cross$pheno$phenotype3 <- rnorm(n = nind(my.cross), sd = my.cross$geno$`2`$data[,15])
+# my.cross$pheno$phenotype4 <- rnorm(n = nind(my.cross), mean = my.cross$geno$`3`$data[,15], sd = my.cross$geno$`3`$data[,15])
+#
+#
+# a1 <- scanonevar(cross = my.cross,
+#                  mean.formula = 'phenotype1 ~ sex + mean.QTL.add + mean.QTL.dom',
+#                  var.formula = '~sex + var.QTL.add + var.QTL.dom',
+#                  return.models = TRUE)$varscan
+# a2 <- scanone(cross = my.cross,
+#               pheno.col = 'phenotype1')
+#
+# b1 <- scanonevar(cross = my.cross,
+#                  mean.formula = 'phenotype2 ~ sex + mean.QTL.add + mean.QTL.dom',
+#                  var.formula = '~sex + var.QTL.add + var.QTL.dom',
+#                  return.models = TRUE)$varscan
+# b2 <- scanone(cross = my.cross,
+#               pheno.col = 'phenotype2')
+#
+# c1 <- scanonevar(cross = my.cross,
+#                  mean.formula = 'phenotype3 ~ sex + mean.QTL.add + mean.QTL.dom',
+#                  var.formula = '~sex + var.QTL.add + var.QTL.dom',
+#                  return.models = TRUE)$varscan
+# c2 <- scanone(cross = my.cross,
+#               pheno.col = 'phenotype3')
+#
+# d1 <- scanonevar(cross = my.cross,
+#                  mean.formula = 'phenotype4 ~ sex + mean.QTL.add + mean.QTL.dom',
+#                  var.formula = '~sex + var.QTL.add + var.QTL.dom',
+#                  return.models = TRUE)$varscan
+# d2 <- scanone(cross = my.cross,
+#               pheno.col = 'phenotype4')
 
 #
 # pdf(file = '../2016_G3_PackageVQTL_Corty/images/LOD_scans.pdf', width = 6, height = 8)
