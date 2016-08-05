03b871c01c42c53ec25ce80988cb1a31017cc6ce
Robert Corty <rcorty@gmail.com>
2016-08-05 14:33:20 -0400

    added is.cross.w.genoprobs
    and documented
    
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/is.cross.R
+++ R/is.cross.R
@@ -79,3 +79,47 @@ is.f2.cross <- function(x) {
 }
 
 
+
+
+#' @title is.cross.w.genoprobs
+#' @name is.cross.w.genoprobs
+#' @author Robert W. Corty \email{rcorty@@gmail.com}
+#'
+#' @inheritParams is.cross
+#'
+#' @return TRUE if x is a cross object with valid genoprobs for each chromosome,
+#' FALSE otherwise
+#'
+#' @export
+#'
+#' @examples
+#' a <- qtl::sim.cross(map = qtl::sim.map())
+#' is.cross.w.genoprobs(x = a)
+#' b <- qtl::calc.genoprob(cross = a)
+#' is.cross.w.genoprobs(x = b)
+#'
+is.cross.w.genoprobs <- function(x) {
+
+  # check that there is a 'prob' element in each chromosome
+  if (!all(sapply(X = x[['geno']], FUN = function(x) 'prob' %in% names(x))))
+    return(FALSE)
+
+  # check that the dimension of each prob element matches the number of individuals
+  is.valid.prob.array <- function(a, cross, chr) {
+    array.dim <- dim(a)
+    if (array.dim[1] != qtl::nind(cross))
+      return(FALSE)
+    if (array.dim[2] < ncol(chr[['data']]))
+      return(FALSE)
+    if (array.dim[3] != 3)
+      return(FALSE)
+    return(TRUE)
+  }
+  if (!all(sapply(X = x[['geno']], FUN = function(x) is.valid.prob.array(a = x[['prob']], cross = x, chr = x))))
+    return(FALSE)
+
+  # all values in the prob array must be between 0 and 1
+  sapply(X = x[['geno']], FUN = function(x) all(x[['prob']] > 0 & x[['prob']] < 1))
+
+  return(is.cross(x))
+}
