13823aa217714caeb92220174b62663690e0a8fe
Robert Corty <rcorty@gmail.com>
2016-08-08 10:43:59 -0400

    many improvements
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/scanonevar_.R
+++ R/scanonevar_.R
@@ -5,10 +5,10 @@
 #' @param modeling.df the tbl_df that contains the response, all covariates,
 #' and NA columns appropriate for marker effects
 #' @param genoprob.df the tbl_df that contains the probability of each genotype
-#' for each individual at each marker or pseudomarker
-#' @param marker.info.df the tbl_df that contains information about the
-#' markers that are being scanned, minimally a 'marker.name', 'chr' (short for
-#' 'chromosome') and 'pos' (short for 'position') for each marker.
+#' for each individual at each loc, where a 'loc' is either a marker or a pseudomarker
+#' @param loc.info.df the tbl_df that contains information about the
+#' locs that are being scanned, minimally a 'loc.name', 'chr' (short for
+#' 'chromosome') and 'pos' (short for 'position') for each loc.
 #' @param scan.types Type(s) of scan(s) to be conducted, one of: 'mean',
 #' 'var', or c('mean', 'var', 'joint')
 #'
@@ -22,73 +22,80 @@
 #'
 scanonevar_ <- function(modeling.df,
                         genoprob.df,
-                        marker.info.df,
+                        loc.info.df,
                         scan.types,
                         scan.formulae) {
 
-  # todo: get rid of this is marker.info.df ends up having just these three cols
-  result <- initialize.scanonevar.result_(marker.info.df,
-                                          scan.types)
+  result <- initialize.scanonevar.result_(loc.info.df, scan.types)
 
   mean.df <- sum(grepl(pattern = 'mean.QTL', x = labels(terms(scan.formulae[['mean.alt.formula']]))))
   var.df <- sum(grepl(pattern = 'var.QTL', x = labels(terms(scan.formulae[['var.alt.formula']]))))
 
   if ('joint' %in% scan.types) {
-    joint.null.fit <- dglm::dglm(formula = scan.formulae[['mean.null.formulae']],
-                                 dformula = scan.formulae[['var.null.formulae']],
-                                 data = modeling.df)
+    joint.null.fit <- tryCatch(expr = dglm::dglm(formula = scan.formulae[['mean.null.formula']],
+                                                 dformula = scan.formulae[['var.null.formula']],
+                                                 data = modeling.df),
+                               warning = function(w) NA,
+                               error = function(e) NA,
+                               finally = NA)
   }
 
 
-  for (marker.idx in 1:nrow(result)) {
+  for (loc.idx in 1:nrow(result)) {
 
-    # fill modeling.df with the genoprobs at the focal marker or pseudomarker
-    marker.name <- result[['marker.name']][marker.idx]
-    marker.genoprobs <- dplyr::filter(.data = genoprob.df,
-                                      marker.name == marker.name)
-    # need to widen this since it's coming in long format
+    # fill modeling.df with the genoprobs at the focal loc
+    this.loc.name <- result[['loc.name']][loc.idx]
+    loc.genoprobs <- dplyr::filter(.data = genoprob.df,
+                                   loc.name == this.loc.name)
 
-    this.marker.modeling.df <- make.marker.specific.modeling.df(general.modeling.df = modeling.df,
-                                                                marker.genoprobs = marker.genoprobs,
-                                                                model.formulae = scan.formulae)
+    this.loc.modeling.df <- make.loc.specific.modeling.df(general.modeling.df = modeling.df,
+                                                          loc.genoprobs = loc.genoprobs,
+                                                          model.formulae = scan.formulae)
 
-    # Fit the alternative model at this marker
-    alternative.fit <- dglm::dglm(formula = scan.formulae[['mean.alt.formula']],
-                                  dformula = scan.formulae[['var.alt.formula']],
-                                  data = this.marker.modeling.df)
+    # Fit the alternative model at this loc
+    alternative.fit <- tryCatch(expr = dglm::dglm(formula = scan.formulae[['mean.alt.formula']],
+                                                  dformula = scan.formulae[['var.alt.formula']],
+                                                  data = this.loc.modeling.df),
+                                warning = function(w) NA,
+                                error = function(e) NA,
+                                finally = NA)
 
 
-    # Fit the appropriate null models at this marker
+    # Fit the appropriate null models at this loc
     # and save LOD score and asymptotic p-value
     if ('mean' %in% scan.types) {
-      mean.null.fit <- dglm::dglm(formula = scan.formulae[['mean.null.formula']],
-                                  dformula = scan.formulae[['var.alt.formula']],
-                                  data = this.marker.modeling.df)
-      result[['mean.lod ']] <- LOD(alt = alternative.fit, null = mean.null.fit)
-      result[['mean.asymp.p']] <- pchisq(q = result[['mean.lod ']], df = mean.df, lower.tail = FALSE)
+      mean.null.fit <- tryCatch(expr = dglm::dglm(formula = scan.formulae[['mean.null.formula']],
+                                                  dformula = scan.formulae[['var.alt.formula']],
+                                                  data = this.loc.modeling.df),
+                                warning = function(w) NA,
+                                error = function(e) NA,
+                                finally = NA)
+      result[['mean.lod']][loc.idx] <- LOD(alt = alternative.fit, null = mean.null.fit)
+      result[['mean.asymp.p']][loc.idx] <- pchisq(q = result[['mean.lod']][loc.idx], df = mean.df, lower.tail = FALSE)
     }
 
     if ('var' %in% scan.types) {
-      var.null.fit <- dglm::dglm(formula = scan.formulae[['mean.alt.formula']],
-                                 dformula = scan.formulae[['var.null.formula']],
-                                 data = this.marker.modeling.df)
-      result[['mean.lod ']] <- LOD(alt = alternative.fit, null = var.null.fit)
-      result[['var.asymp.p']] <- pchisq(q = result[['var.lod ']], df = var.df, lower.tail = FALSE)
+      var.null.fit <- tryCatch(expr = dglm::dglm(formula = scan.formulae[['mean.alt.formula']],
+                                                 dformula = scan.formulae[['var.null.formula']],
+                                                 data = this.loc.modeling.df),
+                               warning = function(w) NA,
+                               error = function(e) NA,
+                               finally = NA)
+      result[['var.lod']][loc.idx] <- LOD(alt = alternative.fit, null = var.null.fit)
+      result[['var.asymp.p']][loc.idx] <- pchisq(q = result[['var.lod']][loc.idx], df = var.df, lower.tail = FALSE)
     }
 
     if ('joint' %in% scan.types) {
-      result[['joint.p']] <- LRS(alt = alternative.fit,
-                                 null = joint.null.fit)
-      result[['joint.lod ']] <- LOD(alt = alternative.fit, null = joint.null.fit)
-      result[['joint.asymp.p']] <- pchisq(q = result[['joint.lod ']], df = mean.df + var.df, lower.tail = FALSE)
+      result[['joint.lod']][loc.idx] <- LOD(alt = alternative.fit, null = joint.null.fit)
+      result[['joint.asymp.p']][loc.idx] <- pchisq(q = result[['joint.lod']][loc.idx], df = mean.df + var.df, lower.tail = FALSE)
     }
 
   }
 
 
   attr(x = result, which = 'scan.types') <- scan.types
-  attr(x = result, which = 'mean.formula') <- mean.formula
-  attr(x = result, which = 'var.formula') <- var.formual
+  attr(x = result, which = 'mean.formula') <- scan.formulae[['mean.formula']]
+  attr(x = result, which = 'var.formula') <- scan.formulae[['var.formula']]
 
   return(result)
 }
@@ -110,24 +117,24 @@ scanonevar_ <- function(modeling.df,
 #' @details This internal function should not typically be called by a user.
 #'
 #' @examples
-#' x <- dplyr::data_frame(marker.name = c('m1', 'm2'),
+#' x <- dplyr::data_frame(loc.name = c('m1', 'm2'),
 #'                        chr = c(1, 1),
 #'                        pos = c(5, 10))
-#' initialize.scanonevar.result_(marker.info.df = x,
+#' initialize.scanonevar.result_(loc.info.df = x,
 #'                               scan.types = 'mean')
 #'
-initialize.scanonevar.result_ <- function(marker.info.df,
+initialize.scanonevar.result_ <- function(loc.info.df,
                                           scan.types) {
 
-  result <- dplyr::select(.data = marker.info.df,
-                          marker.name,
+  result <- dplyr::select(.data = loc.info.df,
+                          loc.name,
                           chr,
                           pos)
 
   if ('mean' %in% scan.types)
     result[['mean.lod']] <- result[['mean.asymp.p']] <- NA
   if ('var' %in% scan.types)
-    result[['var.lod']] <- result[['var.asymp']] <- NA
+    result[['var.lod']] <- result[['var.asymp.p']] <- NA
   if ('joint' %in% scan.types)
     result[['joint.lod']] <- result[['joint.asymp.p']] <- NA
 
@@ -150,6 +157,9 @@ initialize.scanonevar.result_ <- function(marker.info.df,
 #' x <- 27599
 LOD <- function(alt, null) {
 
+  if(any(identical(x = alt, NA), identical(x = null, NA)))
+     return(NA)
+
   stopifnot('dglm' %in% class(alt))
   stopifnot('dglm' %in% class(null))
 
@@ -160,22 +170,64 @@ LOD <- function(alt, null) {
 
 
 
-make.marker.specific.modeling.df <- function(general.modeling.df,
-                                             marker.genoprobs,
-                                             model.formula) {
+make.loc.specific.modeling.df <- function(general.modeling.df,
+                                          loc.genoprobs,
+                                          model.formulae) {
 
-  if ('mean.QTL.add' %in% labels(terms(model.formula[['mean.alt.formula']]))) {
-    general.modeling.df[['mean.QTL.add']] <- additive.component(marker.genoprobs)
+  modeling.df <- general.modeling.df
+
+  if ('mean.QTL.add' %in% labels(terms(model.formulae[['mean.alt.formula']]))) {
+    modeling.df[['mean.QTL.add']] <- additive.component(genoprobs.long = loc.genoprobs)
   }
-  if ('mean.QTL.dom' %in% labels(terms(model.formula[['mean.alt.formula']]))) {
-    general.modeling.df[['mean.QTL.dom']] <- dominance.component(marker.genoprobs)
+  if ('mean.QTL.dom' %in% labels(terms(model.formulae[['mean.alt.formula']]))) {
+    modeling.df[['mean.QTL.dom']] <- dominance.component(genoprobs.long = loc.genoprobs)
   }
-  if ('var.QTL.add' %in% labels(terms(model.formula[['var.alt.formula']]))) {
-    general.modeling.df[['var.QTL.add']] <- additive.component(marker.genoprobs)
+  if ('var.QTL.add' %in% labels(terms(model.formulae[['var.alt.formula']]))) {
+    modeling.df[['var.QTL.add']] <- additive.component(genoprobs.long = loc.genoprobs)
   }
-  if ('var.QTL.dom' %in% labels(terms(model.formula[['var.alt.formula']]))) {
-    general.modeling.df[['var.QTL.dom']] <- dominance.component(marker.genoprobs)
+  if ('var.QTL.dom' %in% labels(terms(model.formulae[['var.alt.formula']]))) {
+    modeling.df[['var.QTL.dom']] <- dominance.component(genoprobs.long = loc.genoprobs)
   }
 
   return(modeling.df)
+}
+
+
+additive.component <- function(genoprobs.long) {
+
+  alleles <- unique(genoprobs.long[['allele']])
+
+  genoprobs.wide <- tidyr::spread(data = genoprobs.long,
+                                  key = allele,
+                                  value = genoprob)
+
+  # genoprobs.wide2 <- dplyr::select(.data = genoprobs.wide, one_of(alleles))
+
+  if (all(alleles %in% c('AA', 'AB', 'BB'))) {
+    return(genoprobs.wide[['AA']] - genoprobs.wide[['BB']])
+  } else if (all(alleles %in% c('g1', 'g2'))) {
+    return(genoprobs.wide[['g2']])
+  } else {
+    stop(paste("Can't determine additive component of loc with alleles:", alleles))
+  }
+}
+
+
+dominance.component <- function(genoprobs.long) {
+
+  alleles <- unique(genoprobs.long[['allele']])
+
+  genoprobs.wide <- tidyr::spread(data = genoprobs.long,
+                                  key = allele,
+                                  value = genoprob)
+
+  # genoprobs.wide2 <- dplyr::select(.data = genoprobs.wide, one_of(alleles))
+
+  if (all(alleles %in% c('AA', 'AB', 'BB'))) {
+    return(genoprobs.wide[['AB']])
+  } else if (all(alleles %in% c('g1', 'g2'))) {
+    return(NA)
+  } else {
+    stop(paste("Can't determine additive component of loc with alleles:", alleles))
+  }
 }
