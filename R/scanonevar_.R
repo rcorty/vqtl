614bb0e35ea78b7fcf51ce6bad1da9d9013b81f7
Robert Corty <rcorty@gmail.com>
2016-08-09 10:49:12 -0400

    reorganizing helper functions
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/scanonevar_.R
+++ R/scanonevar_.R
@@ -180,32 +180,6 @@ initialize.scanonevar.result_ <- function(loc.info.df,
 
 
 
-#' @title LOD
-#' @rdname scanonevar_
-#'
-#' @param alt alternative DGLM
-#' @param null null DGLM
-#'
-#' @return log( likelihood of alternative model / likelihood of null model)
-#' @export
-#'
-#' @examples
-#' x <- 27599
-LOD <- function(alt, null) {
-
-  if(any(identical(x = alt, NA), identical(x = null, NA)))
-     return(NA)
-
-  stopifnot('dglm' %in% class(alt))
-  stopifnot('dglm' %in% class(null))
-
-  return(0.5*(null$m2loglik - alt$m2loglik ))
-}
-
-
-
-
-
 #' @title make.loc.specific.modeling.df
 #' @rdname scanonevar_
 #'
@@ -240,62 +214,3 @@ make.loc.specific.modeling.df <- function(general.modeling.df,
 }
 
 
-
-#' @title additive.component
-#' @rdname scanonevar_
-#'
-#' @param genoprobs.long The genoprobs from which the additive genetic
-#' component should be extracted.  The 'long' format implies that each row
-#' has information on the genoprob of one individual having one allele.
-#'
-#' @return A vector of the additive genetic component at the locus
-#' @export
-#'
-additive.component <- function(genoprobs.long) {
-
-  alleles <- unique(genoprobs.long[['allele']])
-
-  genoprobs.wide <- tidyr::spread(data = genoprobs.long,
-                                  key = allele,
-                                  value = genoprob)
-
-  # genoprobs.wide2 <- dplyr::select(.data = genoprobs.wide, one_of(alleles))
-
-  if (all(alleles %in% c('AA', 'AB', 'BB'))) {
-    return(genoprobs.wide[['AA']] - genoprobs.wide[['BB']])
-  } else if (all(alleles %in% c('g1', 'g2'))) {
-    return(genoprobs.wide[['g2']])
-  } else {
-    stop(paste("Can't determine additive component of loc with alleles:", alleles))
-  }
-}
-
-
-#' @title dominance.component
-#' @rdname scanonevar_
-#'
-#' @param genoprobs.long The genoprobs from which the additive genetic
-#' component should be extracted.  The 'long' format implies that each row
-#' has information on the genoprob of one individual having one allele.
-#'
-#' @return A vector of the dominance genetic component at the locus
-#' @export
-#'
-dominance.component <- function(genoprobs.long) {
-
-  alleles <- unique(genoprobs.long[['allele']])
-
-  genoprobs.wide <- tidyr::spread(data = genoprobs.long,
-                                  key = allele,
-                                  value = genoprob)
-
-  # genoprobs.wide2 <- dplyr::select(.data = genoprobs.wide, one_of(alleles))
-
-  if (all(alleles %in% c('AA', 'AB', 'BB'))) {
-    return(genoprobs.wide[['AB']])
-  } else if (all(alleles %in% c('g1', 'g2'))) {
-    return(0)
-  } else {
-    stop(paste("Can't determine additive component of loc with alleles:", alleles))
-  }
-}
