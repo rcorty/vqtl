c5066c1fc3477d1e3dc4776145eb013c18f97081
Robert Corty <rcorty@gmail.com>
2016-08-08 22:16:16 -0400

    reorganized tests
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- tests/testthat/test-1-utility-functions.R
+++ tests/testthat/test-1-utility-functions.R
@@ -51,3 +51,20 @@ test_that(desc = 'is.f2.cross()',
             z <- qtl::sim.cross(map = qtl::sim.map(), type = 'f2')
             expect_true(object = is.f2.cross(x = z))
           })
+
+
+test_that(
+  desc = 'testing scanonevar',
+  code = {
+    set.seed(27599)
+    test.cross <- qtl::sim.cross(map = qtl::sim.map(len = rep(20, 4), n.mar = 5))
+    test.cross <- qtl::calc.genoprob(cross = test.cross, step = 2)
+
+    # the simplest model
+    sov_result1 <- scanonevar(cross = test.cross)
+
+    expect_false(object = is.scanonevar(x = 3))
+
+    expect_true(object = is.scanonevar(x = sov_result1))
+  }
+)
