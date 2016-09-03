efa1ee6d2ef08d17ca0650c39a373936ef99f0d3
Robert Corty <rcorty@gmail.com>
2016-09-03 10:43:18 -0400

    created effects_plot
    Signed-off-by: Robert Corty <rcorty@gmail.com>

---
+++ R/effects_plot.R
@@ -0,0 +1,24 @@
+effects_plot <- function(sov,
+                         effect.names,
+                         mean.or.var = 'both') {
+
+  validate_effects_plot_inputs(sov, effect.names, mean.or.var)
+
+  wrangle_effects_plot_inputs(sov, effect.names, mean.or.var)
+
+
+
+}
+
+
+validate_effects_plot_inputs <- function(sov, effect.names, mean.or.var) {
+
+  return(TRUE)
+}
+
+
+
+wrangle_effects_plot_inputs <- function(sov, effect.names, mean.or.var) {
+
+  return(TRUE)
+}
