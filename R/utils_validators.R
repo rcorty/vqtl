d5768ed87f5aa2ced927dd5003c3eba122c7163d
Robert Corty <rcorty@gmail.com>
2018-02-07 12:46:14 -0500

    stop on error rather than return FALSE

--- R/utils_validators.R
+++ R/utils_validators.R
@@ -85,12 +85,14 @@ is.scanonevar <- function(x) {
 
 
   # valid result
-  if (!all(c('loc.name', 'chr', 'pos') %in% names(result)))
-    return(FALSE)
+  if (!all(c('loc.name', 'chr', 'pos') %in% names(result))) {
+    stop('loc.name, chr, and pos must be in result')
+  }
 
   if (all(!all(c('mQTL.lod', 'mQTL.asymp.p') %in% names(result)),
-          !all(c('vQTL.lod', 'vQTL.asymp.p') %in% names(result))))
-    return(FALSE)
+          !all(c('vQTL.lod', 'vQTL.asymp.p') %in% names(result)))) {
+    stop('mQTL.lod and mQTL.asymp.p  OR  vQTL.lod and vQTL.asymp.p must be in result')
+  }
 
   # check that LOD scores are greater than 0
 
